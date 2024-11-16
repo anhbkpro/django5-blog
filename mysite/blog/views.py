3ac0c23ba741c12007209d48caf27a9020efb8b0
Anh Lai <laingocanh1990@gmail.com>
2024-11-16 18:16:48 +0700

    ranking results

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -4,7 +4,11 @@ from django.views.decorators.http import require_POST
 from django.views.generic import ListView
 from django.core.mail import send_mail
 from django.db.models import Count
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import (
+    SearchVector,
+    SearchQuery,
+    SearchRank,
+)
 from taggit.models import Tag
 from .forms import CommentForm, EmailPostForm, SearchForm
 from blog.models import Post
@@ -127,9 +131,12 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data["query"]
+            search_vector = SearchVector("title", "body")
+            search_query = SearchQuery(query)
             results = Post.published.annotate(
-                search=SearchVector("title", "body"),
-            ).filter(search=query)
+                search=search_vector,
+                rank=SearchRank(search_vector, search_query),
+            ).filter(search=search_query).order_by("-rank")
     return render(
         request,
         "blog/post/search.html",
